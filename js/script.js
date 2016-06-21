9034bc2194585797d45409039e93f452830faa50
D Ron <Kara@Kara-MacBook-Pro.local>
2016-06-20 22:46:24 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,287 @@
+//STEP 1
+    /*
+    var someMonth
+    function theMonth
+    currentMonth
+    var summerMonth
+    MyLibraryAFunction
+    
+    */
+
+
+
+
+
+//STEP 2 examples of various literal expressions
+    /*
+    numeric = 8
+    string = 'name'
+    boolean = true
+    null = "null" or ' '
+    */
+
+
+
+
+
+//STEP 3 examples of complex variable expressions
+
+/*
+    var jedi = 'Obi-Wan';
+    var age = 38;
+*/
+
+
+
+
+
+
+//STEP 4
+    /*
+    var strFirstName;
+    var strLastName;
+    var strAddress;
+    var strCity;
+    var strState;
+    var numZipCode;
+    var numMyAge;
+    var strReferralSource;
+    var strMayWeContactYou;
+    */
+
+
+
+
+//STEP 5 Example of assigning values
+/*example 1 first assign variable and assign a value
+    var strFirstName;
+    strFirstName = "Dennis";
+    console.log(strFirstName);
+
+
+example 2 assigning a variable and value at the same time  (Creating an Object Literal)
+    var strLastName = "Ronquillo";
+    console.log(strLastName);
+
+// example 3 creating 
+        var strAddress, strCity, strState;
+
+// example 4 using arrays 
+    var strLocation = ["strAddress", "strCity", "strState", "numZipCode"];
+        strLocation[0] = "1234 Taco Bell Avenue";
+        strLocation[1] = "San Diego";
+        strLocation[2] = "CA";           
+        strLocation[3] = "99999";
+    console.log(strLocation);    
+*/ 
+
+
+
+
+
+
+//STEP 6 Coercing values
+
+/*
+    var firstNumber = "20" + 16;
+    var secondNumber = false + "twix";
+    var thirdNumber = true + 35;
+    console.log(thirdNumber);
+*/
+
+
+
+
+
+
+//STEP 7 Demonstrate Hoisting
+
+/*
+alert(greetings);
+var greetings = "Hello out there!";
+*/
+
+
+//STEP 8 fixing the code
+
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+
+
+
+
+//STEP 9 variable produce null value and variable that produces an undefined value
+
+
+/*
+//null value
+        var strOrders1= prompt("Please Click Cancel");
+        console.log(strOrders1);
+
+
+//undefined value
+        var strOrders2;
+        console.log(strOrders2);
+*/
+
+
+
+
+
+//STEP 10 typeof operator to find out the various types of data types 
+
+    /* String
+    var candy = "Twix";
+    window.console.log(typeof candy);
+    */
+
+    /* Numerical
+    var mileage = 33254;
+    window.console.log(typeof mileage);
+    */
+
+    /* Boolean
+    var x = true;
+    window.console.log (typeof x);
+    */
+
+    /* Object
+    var food = new Object();
+    console.log(typeof food);
+    */
+
+    /* Undefined
+    var myWeight;
+    window.console.log (typeof myWeight);
+    */
+
+
+
+//STEP 11 Concatenation operator
+
+/*
+alert('Hello ' + 'Dennis Ronquillo' + ', welcome to the JavaScript class.');
+*/
+
+
+//STEP 12 Concatenation operator using a variable
+
+
+/*
+var name = "Dennis Ronquillo";
+var courseName = "JavaScript";
+alert('Hello ' + name + ', welcome to the JavaScript class.');
+*/
+
+
+//STEP 13 Concatenation operator using 2 variables
+
+/*
+var name = "Dennis Ronquillo";
+var courseName = "JavaScript";
+alert('Hello ' + name + ', welcome to the ' + courseName + ' class.');
+*/
+
+
+
+//STEP 14 creating a line break in the alert box
+
+/*
+var name = "Dennis Ronquillo.";
+var courseName = "JavaScript";
+alert('Hello ' + name  + "\nWelcome to the " + courseName + ' class.');
+*/
+
+
+//STEP 15 Using a Prompt for name (User input)
+/*
+var name = prompt() + '.';
+var courseName = "JavaScript";
+alert('Hello ' + name  + "\nWelcome to the " + courseName + ' class.');
+*/
+
+
+//STEP 16 Using a Prompt for name and courseName (User input)
+
+/* 
+var name = prompt() + '.';
+var courseName = prompt();
+alert('Hello ' + name  + "\nWelcome to the " + courseName + ' class.');
+*/
+
+
+
+//STEP 17 doing math operations 1
+/* 
+var x = 10;
+var y = 20;
+console.log( x + y );
+*/
+
+
+//STEP 18 doing math operations 2
+/* 
+var x = 20 + 20 ;
+console.log( x );
+*/
+
+
+//STEP 19 doing math operations 3
+/* 
+var x = 20 * 5;
+console.log( x );
+*/
+
+
+//STEP 20 doing math operations 4
+/* 
+var x = 2 / 1;
+console.log( x );
+*/
+
+
+//STEP 21 Comparison and Locial Operators True
+/* 
+var x = 1 + 1;
+var y = 2;
+console.log( x == y );
+*/
+
+//STEP 22 Comparison and Locial Operators False
+/*
+var x = 1 + 1;
+var y = 3;
+console.log( x == y );
+ */
+
+
+//STEP 23 
+/* 
+var widget = new Object();
+console.log(typeof widget);
+*/
+
+
+//STEP 24 
+/* 
+    var classmate = ['Cheryl', 'Mike',  'Brad', 'Earl'];
+    window.console.log(classmate instanceof Array); // result is true
+    window.console.log(classmate instanceof Object); // result is true
+*/
+
+
+//STEP 25 
+/* 
+    var classmate = ['Cheryl', 'Mike',  'Brad', 'Earl'];
+    window.console.log(classmate instanceof String); // result is false
+    window.console.log(classmate instanceof Number); // result is false
+*/
+
+
+
+/* Unfinished 
+6, 10, 23
+*/
